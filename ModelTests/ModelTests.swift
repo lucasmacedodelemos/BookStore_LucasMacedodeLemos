e0527137299acd4bf58dee6f8412163f4b97c429
Lucas Macedo de Lemos <lucas.lemos2@br.experian.com>
2020-05-30 15:53:20 -0300

    Added model test.

---
+++ ModelTests/ModelTests.swift
@@ -0,0 +1,24 @@
+//
+//  ModelTests.swift
+//  ModelTests
+//
+//  Created by Lucas Macedo de Lemos on 30/05/20.
+//  Copyright © 2020 Lucas Macedo de Lemos. All rights reserved.
+//
+
+import XCTest
+
+class ModelTests: XCTestCase {
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
